2fcbe2632cf8aa89fb01184459ead0c4cf8c557d
Omar Rehan <omar.se.rehan@gmail.com>
2020-09-03 09:24:57 +0200

    CardButtons binding cleaned

--- Source/MemoryCards/InGameWidget.cpp
+++ Source/MemoryCards/InGameWidget.cpp
@@ -6,20 +6,12 @@ void UInGameWidget::Setup() {
 		GameMode->SetNumOfMovesTextBlock(NumberOfMovesTextBlock);
 	}
 
-	if (CardButton_0)
-		CardButton_0->Initialize();
-	if (CardButton_1)
-		CardButton_1->Initialize();
-	if (CardButton_2)
-		CardButton_2->Initialize();
-	if (CardButton_3)
-		CardButton_3->Initialize();
-	if (CardButton_4)
-		CardButton_4->Initialize();
-	if (CardButton_5)
-		CardButton_5->Initialize();
-	if (CardButton_6)
-		CardButton_6->Initialize();
-	if (CardButton_7)
-		CardButton_7->Initialize();
+	if (CardsGrid) {
+		int32 NumOfCards = CardsGrid->GetChildrenCount();
+		for (int32 Index = 0; Index < NumOfCards; Index++) {
+			UCardButton* Card = Cast<UCardButton>(CardsGrid->GetChildAt(Index));
+			Card->Initialize();
+			CardButtons.Add(Card);
+		}
+	}
 }
--- Source/MemoryCards/InGameWidget.h
+++ Source/MemoryCards/InGameWidget.h
@@ -8,6 +8,7 @@
 #include "Blueprint/UserWidget.h"
 #include "Kismet/GameplayStatics.h"
 #include "MemoryCardsGameModeBase.h"
+#include "Components/UniformGridPanel.h"
 #include "InGameWidget.generated.h"
 
 UCLASS()
@@ -18,22 +19,11 @@ public:
 		virtual void Setup() override final;
 
 protected:
-	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
 		UTextBlock* NumberOfMovesTextBlock;
-	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
-		UCardButton* CardButton_0;
-	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
-		UCardButton* CardButton_1;
-	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
-		UCardButton* CardButton_2;
-	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
-		UCardButton* CardButton_3;
-	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
-		UCardButton* CardButton_4;
-	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
-		UCardButton* CardButton_5;
-	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
-		UCardButton* CardButton_6;
-	UPROPERTY(VisibleAnywhere, meta = (BindWidget))
-		UCardButton* CardButton_7;
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+		UUniformGridPanel* CardsGrid;
+
+	UPROPERTY()
+		TArray<UCardButton*> CardButtons;
 };
